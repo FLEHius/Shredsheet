192a3f5d36b473641a1c11c8f7c0861dfe1a5e1d
Fleh <felix.koch5@hotmail.com>
2023-05-23 05:54:28 +0200

    i have committed code warcrimes, please don't judge. kthx

--- src/js/filterslider.js
+++ src/js/filterslider.js
@@ -1,16 +1,93 @@
-const getVals = () => 
-([sliderRange1, sliderRange2, sliderRange3]).map(item => item.value);
+var slider0 = document.getElementById("sliderRange0")
+var output0 = document.getElementById("slideroutput0")
+var textinput0 = document.getElementById("slideroutput0")
+output0.value = slider0.value;
+
+slider0.oninput = function(){
+  output0.value = this.value;
+}
 
 
-const updateValue = (values) => {
-    const [v1, v2, v3] = values;
-    return [slideroutput1.textContent, 
-        slideroutput2.textContent, slideroutput3.textContent] = [v1, v2, v3];
-  }
+var slider1 = document.getElementById("sliderRange1")
+var output1 = document.getElementById("slideroutput1")
+output1.value = slider1.value;
 
-  container.addEventListener("input", (e) => {
-    if (e.target && e.target.nodeName == "INPUT") {
-      const values = getVals();
-      updateValue(values);
-    }
-  });
+
+slider1.oninput = function(){
+  output1.value = this.value;
+}
+
+var slider2 = document.getElementById("sliderRange2")
+var output2 = document.getElementById("slideroutput2")
+output2.value = slider0.value;
+
+slider2.oninput = function(){
+  output2.value = this.value;
+}
+
+
+var slider3 = document.getElementById("sliderRange3")
+var output3 = document.getElementById("slideroutput3")
+output3.value = slider1.value;
+
+
+slider3.oninput = function(){
+  output3.value = this.value;
+}
+
+var slider4 = document.getElementById("sliderRange4")
+var output4 = document.getElementById("slideroutput4")
+output4.value = slider4.value;
+
+
+slider4.oninput = function(){
+  output4.value = this.value;
+}
+
+var slider5 = document.getElementById("sliderRange5")
+var output5 = document.getElementById("slideroutput5")
+output5.value = slider0.value;
+
+slider5.oninput = function(){
+  output5.value = this.value;
+}
+
+
+var slider6 = document.getElementById("sliderRange6")
+var output6 = document.getElementById("slideroutput6")
+output6.value = slider6.value;
+
+
+slider6.oninput = function(){
+  output6.value = this.value;
+}
+
+var slider7 = document.getElementById("sliderRange7")
+var output7 = document.getElementById("slideroutput7")
+output6.value = slider6.value;
+
+
+slider7.oninput = function(){
+  output7.value = this.value;
+}
+
+var slider8 = document.getElementById("sliderRange8")
+var output8 = document.getElementById("slideroutput8")
+output8.value = slider8.value;
+
+
+slider8.oninput = function(){
+  output8.value = this.value;
+}
+
+var slider9 = document.getElementById("sliderRange9")
+var output9 = document.getElementById("slideroutput9")
+output9.value = slider9.value;
+
+
+slider9.oninput = function(){
+  output9.value = this.value;
+}
+
+
+
